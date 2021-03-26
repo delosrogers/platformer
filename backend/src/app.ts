6de4ec2ef273f91513c047e4184b774e88ce8d8d
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-03-25 22:51:37 -0700

    Implemented basic google login
    Signed-off-by: Mattias de los Rios Rogers <delosrogers@gmail.com>

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,32 +1,104 @@
 import express from 'express';
 import path from 'path';
-import mongoose, { connect } from 'mongoose';
+import { connect } from 'mongoose';
+import mongoose = require('mongoose')
 import { model, Schema, Model, Document } from 'mongoose';
+import passport from 'passport';
+import Google from 'passport-google-oauth';
+import findOrCreate from 'mongoose-findorcreate'
+import cookieSession from 'cookie-session'
+import cryptoRandomString from 'crypto-random-string'
+
+const GoogleStrategy = Google.OAuth2Strategy;
+
+require('dotenv').config();
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback",
+},
+    function (accessToken: string, refreshToken: string, profile, done) {
+        connect('mongodb://localhost:27017/platformer', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        User.findOne({ googleId: profile.id }, (err, user) => {
+            if (user) {
+                done(err, user);
+            } else {
+                User.create({
+                    googleId: profile.id,
+                    name: profile.displayName,
+                    highScore: 0,
+                }, (err, user) => {
+                    done(err, user);
+                });
+            }
+        });
+    }
+
+));
+
+mongoose.set('toJSON', { transform: true, flattenDecimals: true });
 
 interface IUser extends Document {
     name: string,
-    highScore: number
+    highScore: number,
+    googleId: string,
 }
 
 const UserSchema: Schema = new Schema({
     name: { type: String, required: true },
     highScore: { type: Number, required: true },
+    googleId: { type: String, required: true },
 });
+
+UserSchema.plugin(findOrCreate);
 
 const User: Model<IUser> = model('User', UserSchema);
 
 
 const app = express();
 app.use(express.json());
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [cryptoRandomString({ length: 20 })]
+}));
+app.use(passport.initialize())
+app.use(passport.session());
+
+
+
+passport.serializeUser(function (user: IUser, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id)
+        .then(
+            (user) => done(null, user)
+        );
+});
+
 
 const port = 3000;
 app.get('/', (req, res) => {
+    console.log('current user', req.user)
     res.sendFile(path.join(__dirname + '/static/elm.html'));
 });
 
 app.get('/elm.js', (req, res) => {
     res.sendFile(path.join(__dirname + '/static/elm.js'));
 });
+
+app.get('/auth/google',
+    passport.authenticate(
+        'google',
+        { scope: ['https://www.googleapis.com/auth/plus.login', 'profile', 'email'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => res.redirect('/'));
 
 app.get('/api/v1/u/:id', async (req, res) => {
     const userID = req.params.id;
