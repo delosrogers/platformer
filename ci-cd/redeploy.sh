0efb9226e639bfe957542657a4a508f47f1337bf
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-03-28 15:05:27 -0700

    env vars in redeploy

--- ci-cd/redeploy.sh
+++ ci-cd/redeploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 git pull
+. .env
+export PRIVKEYPATHNODOCKER=~/certs/platformer.genedataexplorer.space/privkey.pem
+export PRIVKEYPATHNODOCKER=~/certs/platformer.genedataexplorer.space/fullchain.pem
 docker compose down
 docker compose build
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
