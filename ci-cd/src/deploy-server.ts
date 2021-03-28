dc598ddb8435d6bcc052c175659cdae4d5bab0e4
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-03-28 15:24:24 -0700

    fix

--- ci-cd/src/deploy-server.ts
+++ ci-cd/src/deploy-server.ts
@@ -9,7 +9,7 @@ const privateKey = fs.readFileSync(process.env.PRIVKEYPATHNODOCKER, 'utf8');
 const certificate = fs.readFileSync(process.env.CERTPATHNODOCKER, 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(3001, "0.0.0.0", () => console.log("listening on port 3001"));
+httpsServer.listen(3001, "platformer.genedataexplorer.space", () => console.log("listening on port 3001"));
 
 
 app.post('/deploy', (_, res) => {
