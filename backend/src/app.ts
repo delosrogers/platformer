c446bfff1453d1d53b7df36eef612f822bdf74a3
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-03-28 14:44:41 -0700

    try to see if changes work

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -162,7 +162,7 @@ app.put('/api/v1/u/:id/highscore', csrfProtection, async (req, res) => {
     const score: number = req.body.score;
     try {
         await newHighScore(score, id);
-        res.sendStatus(200);
+        res.send("success you saved your score");
     } catch (e) {
         console.log(e.message);
         if (e.message == "No Such User") {
