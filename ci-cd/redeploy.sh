cdd31bc4b348dac02725dc3abfd8e6897f6faab8
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-03-28 12:07:24 -0700

    fixed compose script

--- ci-cd/redeploy.sh
+++ ci-cd/redeploy.sh
@@ -3,4 +3,4 @@
 git pull
 docker compose build
 docker compose down
-docker commpose up -f docker-complse.yml -f docker-compose.prod.yml -d
+docker commpose -f docker-complse.yml -f docker-compose.prod.yml -d up
