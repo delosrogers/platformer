8248106f31b96258df0636ea704b53f787a75001
Mattias de los Rios Rogers <delosrogers@gmail.com>
2021-03-28 11:13:50 -0700

    created ci cd server

---
+++ ci-cd/src/deploy-server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { exec } from 'child_process';
+import fs from 'fs';
+import https from 'https';
+
+const app = express();
+
+const privateKey = fs.readFileSync(process.env.PRIVKEYPATH, 'utf8');
+const certificate = fs.readFileSync(process.env.CERTPATH, 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(3001, "platformer.genedataexplorer.space", () => console.log("listening on port 3001"));
+
+
+app.post('/deploy', (_, res) => {
+    exec("ci-cd/redeploy.sh");
+    res.sendStatus(200);
+})
